84a631dd6b32baf6b41a5b70711cae3ef5fa1968
Anan Wenwen <admin@antwen.cn>
2026-09-12 19:44:24 +0800

    feat: update background music to The Setup

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -48,10 +48,10 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				name: "The Setup",
+				artist: "Favored Nations",
+				url: "/assets/music/the-setup-music.mp3",
+				cover: "/assets/music/cover/the-setup-img.jpg",
 				lrc: "",
 			},
 		],
